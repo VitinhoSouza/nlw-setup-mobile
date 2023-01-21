b97ffa85c922fa9f44a04f505674a8ee47f4098a
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2023-01-21 13:14:49 -0300

    Class 4 - Connecting to the api

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -5,6 +5,7 @@ import colors from "tailwindcss/colors";
 
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
+import { api } from "../lib/axios";
 
 const availableWeekDays = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
 
@@ -17,6 +18,25 @@ export function New() {
       setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex));
     } else {
       setWeekDays(prevState => [...prevState, weekDayIndex]);
+    }
+  }
+
+  async function handleCreateNewHabit() {
+    try {
+      if (!title.trim() || weekDays.length === 0) {
+        Alert.alert("Novo hábito", "Informe o nome do hábito e escolha a periodicidade.");
+        return;
+      }
+
+      await api.post("/habits", { title, weekDays });
+      
+      setTitle("");
+      setWeekDays([]);
+
+      Alert.alert("Novo hábito", "Hábito criado com sucesso!");
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Ops", "Não foi possível criar o novo hábito");
     }
   }
 
@@ -62,6 +82,7 @@ export function New() {
         <TouchableOpacity
           className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
           activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
         >
           <Feather 
            name="check"
